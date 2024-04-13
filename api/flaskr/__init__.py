614a665fc41bbdba85ea2cb27c09561c0020df2a
austinmpask <austinmpask@gmail.com>
2024-04-13 16:31:57 -0700

    pg db init

--- api/flaskr/__init__.py
+++ api/flaskr/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .config import Configuration
 
+# Config flask
 app = Flask(__name__)
+app.config.from_object(Configuration)
+
+# Init DB
+db = SQLAlchemy(app)
 
 
 @app.route("/")

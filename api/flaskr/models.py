795346cc3474d929022d7170a894ef6dcaaef6a2
austinmpask <austinmpask@gmail.com>
2024-04-15 13:50:48 -0700

    init model constraints

--- api/flaskr/models.py
+++ api/flaskr/models.py
@@ -3,6 +3,7 @@ from sqlalchemy.types import String, Integer, DateTime, Date, Boolean
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 db = SQLAlchemy()
@@ -16,8 +17,8 @@ usernameLen = 20
 class Domain(db.Model):
     __tablename__ = "domains"
     id = Column(Integer, primary_key=True)
-    domain_name = Column(String(genStringLen))
-    created_at = Column(DateTime, server_default=func.now())
+    domain_name = Column(String(genStringLen), nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
 
     users = relationship("User", back_populates="domain")
 
@@ -28,12 +29,12 @@ class Domain(db.Model):
 class User(db.Model):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    user_name = Column(String(usernameLen))
-    email = Column(String(emailLen))
-    password_hash = Column(String(hashLen))
-    created_at = Column(DateTime, server_default=func.now())
+    user_name = Column(String(usernameLen), nullable=False)
+    email = Column(String(emailLen), nullable=False)
+    password_hash = Column(String(hashLen), nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
 
-    domain_id = Column(Integer, ForeignKey("domains.id"))
+    domain_id = Column(Integer, ForeignKey("domains.id"), nullable=False)
     domain = relationship("Domain", back_populates="users")
 
     projects = relationship("UserProject", back_populates="user")
@@ -42,21 +43,21 @@ class User(db.Model):
 class ProjectType(db.Model):
     __tablename__ = "project_types"
     id = Column(Integer, primary_key=True)
-    project_type = Column(String(genStringLen))
+    project_type = Column(String(genStringLen), nullable=False)
 
 
 class UserProject(db.Model):
     __tablename__ = "user_projects"
     id = Column(Integer, primary_key=True)
-    user_project_name = Column(String(genStringLen))
-    user_project_issuance = Column(Date)
-    user_project_budget = Column(Integer)
-    user_project_billed = Column(Integer)
-    created_at = Column(DateTime)
+    user_project_name = Column(String(genStringLen), nullable=False)
+    user_project_issuance = Column(Date, nullable=False)
+    user_project_budget = Column(Integer, nullable=False, default=0)
+    user_project_billed = Column(Integer, nullable=False, default=0)
+    created_at = Column(DateTime, default=datetime.now())
 
-    project_type_id = Column(Integer, ForeignKey("project_types.id"))
+    project_type_id = Column(Integer, ForeignKey("project_types.id"), nullable=False)
 
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     user = relationship("User", back_populates="projects")
 
     sections = relationship("ProjectSection", back_populates="user_project")
@@ -65,21 +66,21 @@ class UserProject(db.Model):
 class SectionType(db.Model):
     __tablename__ = "section_types"
     id = Column(Integer, primary_key=True)
-    section_type = Column(String(genStringLen))
-    default_section_number = Column(Integer)
+    section_type = Column(String(genStringLen), nullable=False)
+    default_section_number = Column(Integer, nullable=False)
 
 
 class ProjectSection(db.Model):
     __tablename__ = "project_sections"
     id = Column(Integer, primary_key=True)
-    section_number = Column(Integer)
+    section_number = Column(Integer, nullable=False)
     custom_section_type = Column(String(genStringLen))
 
-    created_at = Column(DateTime)
+    created_at = Column(DateTime, default=datetime.now())
 
-    section_type_id = Column(Integer, ForeignKey("section_types.id"))
+    section_type_id = Column(Integer, ForeignKey("section_types.id"), nullable=False)
 
-    user_project_id = Column(Integer, ForeignKey("user_projects.id"))
+    user_project_id = Column(Integer, ForeignKey("user_projects.id"), nullable=False)
     user_project = relationship("UserProject", back_populates="sections")
 
     line_items = relationship("LineItem", back_populates="project_section")
@@ -88,9 +89,11 @@ class ProjectSection(db.Model):
 class LineItem(db.Model):
     __tablename__ = "line_items"
     id = Column(Integer, primary_key=True)
-    flag = Column(Boolean)
-    item = Column(String(genStringLen))  # to be updated
-    created_at = Column(DateTime)
+    flag = Column(Boolean, nullable=False, default=False)
+    item = Column(String(genStringLen), nullable=False, default="")  # to be updated
+    created_at = Column(DateTime, default=datetime.now())
 
-    project_section_id = Column(Integer, ForeignKey("project_sections.id"))
+    project_section_id = Column(
+        Integer, ForeignKey("project_sections.id"), nullable=False
+    )
     project_section = relationship("ProjectSection", back_populates="line_items")

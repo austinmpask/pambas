3b3e427f038902e43e08b2e5bd94da798106ab80
austinmpask <austinmpask@gmail.com>
2024-05-14 13:45:24 -0700

    refactor unit tests for user service models

--- user-service/tests/unit/test_models.py
+++ user-service/tests/unit/test_models.py
@@ -35,10 +35,19 @@ def sample_user(app):
 def test_timestamp_user(app, sample_user):
     """User records should automatically include a timestamp"""
     user = User(**sample_user)
-    with app.app_context():
-        db.session.add(user)
+
+    db.session.add(user)
+    db.session.commit()
+    assert user.created_at is not None
+
+
+def test_uuid_is_uuid(app, sample_user):
+    """Non UUID types should not be valid"""
+    sample_user["uuid"] = "asdfasdfasdf"
+    user = User(**sample_user)
+    db.session.add(user)
+    with pytest.raises(DataError):
         db.session.commit()
-        assert user.created_at is not None
 
 
 def test_unique_uuid(app, sample_user):
@@ -46,47 +55,42 @@ def test_unique_uuid(app, sample_user):
     user1 = User(**sample_user)
     user2 = User(**sample_user)
 
-    with app.app_context():
-        db.session.add_all([user1, user2])
-        with pytest.raises(IntegrityError):
-            db.session.commit()
+    db.session.add_all([user1, user2])
+    with pytest.raises(IntegrityError):
+        db.session.commit()
 
 
 def test_non_nullable_first(app, sample_user):
     """First name should be non nullable"""
     del sample_user["firstName"]
     user = User(**sample_user)
-    with app.app_context():
-        db.session.add(user)
-        with pytest.raises(IntegrityError):
-            db.session.commit()
+    db.session.add(user)
+    with pytest.raises(IntegrityError):
+        db.session.commit()
 
 
 def test_non_nullable_last(app, sample_user):
     """Last name should be non nullable"""
     del sample_user["lastName"]
     user = User(**sample_user)
-    with app.app_context():
-        db.session.add(user)
-        with pytest.raises(IntegrityError):
-            db.session.commit()
+    db.session.add(user)
+    with pytest.raises(IntegrityError):
+        db.session.commit()
 
 
 def test_max_length_first_name(app, sample_user):
     """First name should adhere to a maximum length of 20 characters"""
     sample_user["firstName"] = "a" * 21
     user = User(**sample_user)
-    with app.app_context():
-        db.session.add(user)
-        with pytest.raises(DataError):
-            db.session.commit()
+    db.session.add(user)
+    with pytest.raises(DataError):
+        db.session.commit()
 
 
 def test_max_length_last_name(app, sample_user):
     """Last name should adhere to a maximum length of 20 characters"""
     sample_user["lastName"] = "a" * 21
     user = User(**sample_user)
-    with app.app_context():
-        db.session.add(user)
-        with pytest.raises(DataError):
-            db.session.commit()
+    db.session.add(user)
+    with pytest.raises(DataError):
+        db.session.commit()

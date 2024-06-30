ea50c73971b9b6bcd755483d25a4064f94821951
austinmpask <austinmpask@gmail.com>
2024-06-30 15:53:59 -0700

    shallowdelete fix auth service

--- auth-service/flaskr/routes/shallowdelete.py
+++ auth-service/flaskr/routes/shallowdelete.py
@@ -1,38 +1,40 @@
-from flask import request, Blueprint
-from flaskr.models import db, User
-from flaskr.utils import sendJsonResponse, jsonRequired, queryForUserByUUID
-import uuid
+from flask import Blueprint
+from flaskr.models import db
+from flaskr.utils import (
+    sendJsonResponse,
+    jsonRequired,
+    uuidRequired,
+    queryForUserByUUID,
+)
 
 shallowDeleteBP = Blueprint("shallowDelete", __name__)
 
 
-# Delete user data ONLY from the auth db. This is not an application wide user deletion
+# Delete user data ONLY from the auth db. This is not an application wide user deletion.
+# Used if there is a registration error between services
 @shallowDeleteBP.route("/shallowdelete", methods=["DELETE"])
 @jsonRequired
-def shallowDelete():
-
-    data = request.get_json()
-
-    # Convert to UUID object, but catch error if request uuid is invalid format
-    try:
-        user_uuid = uuid.UUID(data.get("uuid"))
-    except Exception:
-        return sendJsonResponse(400, "Invalid UUID")
+@uuidRequired(True)
+def shallowDelete(userUUID):
 
     # Lookup user by UUID
-    user = queryForUserByUUID(user_uuid)
+    status, body = queryForUserByUUID(userUUID)
 
-    # Handle errors from helper function
-    if not isinstance(user, User):
-        return sendJsonResponse(*user)
-    # If there is a match, delete the db entry
+    # Abort if error or user not found
+    if status != 200:
+        return sendJsonResponse(status, body or "User not found")
+
+    # If there is a match attempt to delete the db entry
     try:
-        db.session.delete(user)
+        db.session.delete(body)
         db.session.commit()
+
         # Successful deletion of user, send OK response
-        return sendJsonResponse(200, user.uuid)
+        return sendJsonResponse(200, userUUID)
 
     except Exception as e:
         # If there is a DB error, rollback and forward the error in response
         db.session.rollback()
-        return sendJsonResponse(500, "Error from auth DB", e)
+        return sendJsonResponse(
+            500, f"Auth database error while deleting User: {str(e)}"
+        )
